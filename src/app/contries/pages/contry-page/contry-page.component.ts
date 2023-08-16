eb44bcb8cbbf74573257c22023d6c1d2557dc072
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-08-16 10:48:36 -0600

    swithmap

--- src/app/contries/pages/contry-page/contry-page.component.ts
+++ src/app/contries/pages/contry-page/contry-page.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ContrySevices } from '../../services/contries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-contry-page',
   templateUrl: './contry-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class ContryPageComponent {
+export class ContryPageComponent implements OnInit {
+  constructor(
+    private activatedRouer: ActivatedRoute,
+    private router: Router,
+    private contryServices: ContrySevices,
+    ) {}
+
+
+  ngOnInit(): void {
+    this.activatedRouer.params
+    .pipe(
+      switchMap(({id}) => this.contryServices.searchCountryByAlphaCode(id) )
+    )
+    .subscribe((resp) => {
+        if(!resp) {
+          return this.router.navigateByUrl('')
+        }
+        console.log('Tenemos un pais');
+
+        return
+        
+    });
+  }
+
 
 }
