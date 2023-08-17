ea345a7ca9d29ea2280a0b2e326a7fcacf0f29bc
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-08-17 13:30:37 -0600

    searchforRegions

--- src/app/contries/pages/by-region-page/by-region-page.component.ts
+++ src/app/contries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ContrySevices } from '../../services/contries.service';
 import { Country } from '../../interfaces/country';
 
+type Region= 'Africa'| 'Americas'| 'Asia'| 'Europe'| 'Oceania'
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,11 +12,18 @@ import { Country } from '../../interfaces/country';
 export class ByRegionPageComponent {
 
   public region : Country[]=[]
+
+  public regions: Region[]= ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  public selectedRegion?: Region;
+
   constructor( public countryServices: ContrySevices){
 
   }
 
-  searchRegion(term: string){
+  searchRegion(term: Region){
+
+    this.selectedRegion= term;
     this.countryServices.searchRegion(term)
     .subscribe(region=>{
       this.region = region
