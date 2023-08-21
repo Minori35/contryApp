1477e9147f4cfc3c06cd02271e4293c3bfafaf04
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-08-21 11:24:51 -0600

    localStoragre

--- src/app/contries/services/contries.service.ts
+++ src/app/contries/services/contries.service.ts
@@ -10,6 +10,16 @@ export class ContrySevices {
 
     private apiUrl : string ='https://restcountries.com/v3.1'
 
+    private saveToLocalStorage(){
+        localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore) )
+    }
+
+    private leadFromLocalStorage(){
+        if(!localStorage.getItem('cacheStore')) return;
+
+        this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!)
+    }   
+
     public cacheStore: CacheStore ={
         byCapital:{term : '', country:[]},
         byCountry:{term:'',country : []},
@@ -19,6 +29,7 @@ export class ContrySevices {
 
 
     private getCountriespRequest(url : string):Observable<Country[]>{
+        console.log("🚀 ~ file: contries.service.ts:33 ~ ContrySevices ~ getCountriespRequest ~ url:", url)
         return this.http.get<Country[]>(url)
         .pipe(
             catchError(() => of([])),
@@ -26,7 +37,9 @@ export class ContrySevices {
         );
     }
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+        this.leadFromLocalStorage();
+     }
 
     searchCountryByAlphaCode(code: string): Observable<Country | null>{
         return this.http.get<Country[]>(`${ this.apiUrl}/alpha/${code}`)
@@ -40,7 +53,8 @@ export class ContrySevices {
         const url= `${ this.apiUrl}/capital/${term}`;
         return this.getCountriespRequest(url)
         .pipe(
-            tap(country => this.cacheStore.byCapital= {term , country} )
+            tap(country => this.cacheStore.byCapital= {term , country} ),
+            tap( () => this.saveToLocalStorage()),
         );
 
     }
@@ -49,9 +63,9 @@ export class ContrySevices {
         const url= `${ this.apiUrl}/name/${term}`;
         return this.getCountriespRequest(url).
         pipe(
-            tap(country => {
-                return this.cacheStore.byCountry= {term , country} 
-            })
+            tap(country => this.cacheStore.byCountry= {term , country} ),
+
+            tap( () => this.saveToLocalStorage()),
         );
     }
 
@@ -61,7 +75,8 @@ export class ContrySevices {
         pipe(
             tap(country => {
                 return this.cacheStore.byRegion= {region , country} 
-            })
+            }),
+            tap( () => this.saveToLocalStorage()),
         );
     }
 }
